b081645974cbb4e12da0e90925c054219640c881
Aaaaaalice2206 <lg4049@nyu.edu>
2025-12-10 16:40:35 +0800

    1

--- little-red/shutdown.js
+++ little-red/shutdown.js
@@ -1,10 +1,16 @@
-let trigger = document.querySelector(".phoneontheground");
+let phone = document.querySelector("#phoneontheground");
 let popup = document.querySelector(".phone-popup");
 
-trigger.addEventListener("click", function() {
-    popup.style.display = "flex";
+phone.addEventListener("click", function() {
+    popup.style.display = "block";
 });
 
 popup.addEventListener("click", function() {
     popup.style.display = "none";
+});
+
+let wolf = document.querySelector(".wolf");
+wolf.style.cursor = "pointer"; 
+wolf.addEventListener("click", function(){
+    window.location.href = "lesson.html"; 
 });
